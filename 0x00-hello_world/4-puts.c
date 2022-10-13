287c36e147e4f8595314709b25987996ca906ea3
ssebugenyip <ssebugenyipatric7@gmail.com>
2022-10-13 10:00:58 -0700

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+# !bin / bash
+#include <stdio.h>
+/**
+* main - Starting point of the program execution
+* Description: the program's description
+* Return: Always 0 (Indicating Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
