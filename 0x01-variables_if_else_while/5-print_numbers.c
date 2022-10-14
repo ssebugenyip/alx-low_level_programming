dc44294355f7e2f78973f80d1e913b8c7f291fba
ssebugenyip <ssebugenyipatric7@gmail.com>
2022-10-14 15:40:15 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - starting point
+*description: Numbers
+*Return: Always 0 success
+*/
+int main(void)
+{
+	int y = 0;
+
+	for (y = 0; y < 10; y++)
+	{
+		printf("%d\n", y);
+	
+	}
+
+
+
+	return (0);
+}
