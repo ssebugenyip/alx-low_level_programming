f666f64119dc246ab97c1ff4be7ca5e5867a45fb
ssebugenyip <ssebugenyipatric7@gmail.com>
2022-10-14 15:58:28 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+*main - starting point
+*description 'program that prints in alphabet'
+*Return: Always 0 for success
+*/
+int main(void)
+{
+	int y = 0;
+
+	for (y = 0; y < 10; y++)
+	{
+		putchar(y);
+
+	}
+	putchar('\n');
+
+
+	return (0);
+}
+
