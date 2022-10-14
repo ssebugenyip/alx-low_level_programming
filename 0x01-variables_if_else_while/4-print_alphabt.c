e1ffec5ea2a509cc151996943506546804bc5b36
ssebugenyip <ssebugenyipatric7@gmail.com>
2022-10-14 14:41:43 -0700

    Alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+*main - start of program
+*description - prints alphabet
+*Return: Always 0 success
+*/
+int main(void)
+{
+	char alphabet = 'A';
+
+	while (alphabet = 'A' && alphabet < 'E' && alphabet < 'Q' && alphabet <
+	='Z')
+	{
+		putchar(tolower(alphabet));
+		alphabet++;
+
+
+	}
+	putchar('\n');
+
+	return (0);
+
+}
