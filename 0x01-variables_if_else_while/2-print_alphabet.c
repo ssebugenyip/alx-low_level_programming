6a6908f777370fed1e524961e8037bbb23036cf1
ssebugenyip <ssebugenyipatric7@gmail.com>
2022-10-14 12:03:58 -0700

    Alphabet lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - starting point 
+*description 'program that prints in alphabet'
+*Return: Always 0 for success
+*/
+int main(void)
+{
+	char x;
+
+	while (x = 'A' && x <= 'Z')
+	{
+		putchar(tolower(x));
+
+	}
+
+
+
+
+
+	return (0);
+}
