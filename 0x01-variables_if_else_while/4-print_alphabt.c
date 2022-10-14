d30f27c31f8d8e6a86db9d3d1009c9c2a52ffb71
ssebugenyip <ssebugenyipatric7@gmail.com>
2022-10-14 15:24:21 -0700

    Alphabet soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,10 +13,12 @@ int main(void)
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++) 
 	{
 		if (alphabet == 'e' || alphabet == 'q')
+		{
 			continue;
 			putchar(alphabet);
-
 		}
+
+	}
 
 	putchar('\n');
 
