fe35aef3da2dae2203bcb6a541329cfb54fa8682
ssebugenyip <ssebugenyipatric7@gmail.com>
2022-10-14 17:24:08 -0700

    PPPPP

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+i#include <stdio.h>
+/**
+*main - starting point of program
+*description 'All possible combinations of single digit numbers'
+*Return: Always 0 success
+*/
+int main(void)
+{
+	int y = 0;
+
+	if (y < 10)
+	{
+		putchar((y % 10) + '0');
+		while (y == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+
+	}
+	putchar('\n');
+
+
+
+
+	return (0);
+}
