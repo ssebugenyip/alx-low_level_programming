2358dd45c084e42fb5da1eef29a73504ab5cc888
ssebugenyip <ssebugenyipatric7@gmail.com>
2022-10-14 18:59:57 -0700

    all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,14 +15,12 @@ int main(void)
 		{
 			putchar((x % 10) + '0');
 			putchar((y % 10) + '0');
-			if (x == 8 && y == 9)			
+			if (x == 8 && y == 9)
 				continue;
 			putchar(',');
 			putchar(' ');
 		}
-	
 	}
 	putchar('\n');
 	return (0);
-	
 }
