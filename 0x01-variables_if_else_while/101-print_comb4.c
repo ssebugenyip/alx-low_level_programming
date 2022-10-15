0cc473a9d3f9eb6fb134387232fbac86fe00a5d0
ssebugenyip <ssebugenyipatric7@gmail.com>
2022-10-14 19:18:30 -0700

    prints all possible different combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+*main - starting point
+*description 'different combinations of two numbers'
+*Return: Always 0 success
+*/
+int main(void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = x + 1; y < 10; y++)
+		{
+			for (z = y + 1; z < 10; z++)
+			{
+				putchar((x % 10) + '0');
+				putchar((y % 10) + '0');
+				putchar((z % 10) + '0');
+
+				if (x == 0 && y == 1 && z == 2)
+					continue;
+
+				putchar(',');
+				putchar(' ');
+
+			}
+
+
+		}
+
+
+
+	}
+
+	putchar('\n');
+
+
+	return (0);
+}
