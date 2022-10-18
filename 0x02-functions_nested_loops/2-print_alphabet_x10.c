def7964dafac0edfd88fcbff6b229d998aaa81d0
ssebugenyip <ssebugenyipatric7@gmail.com>
2022-10-18 12:21:41 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*print _alphabet - '_alphabet_x10'
+*print_alphabet_x10 - '_alphabet_x10'
 *description 'program that prints 10times the alphabet, in lowercase, followed
 *by a new line'
 */
@@ -14,7 +14,7 @@ void print_alphabet_x10(void)
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 	}
-	_putchar('\n');
+		_putchar('\n');
 
 
 }
