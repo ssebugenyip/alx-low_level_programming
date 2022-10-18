6f2b03e17ab58017df2350cbf6d8f93f0ccfe1d1
ssebugenyip <ssebugenyipatric7@gmail.com>
2022-10-18 11:28:17 -0700

    _alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+*main - starting point of the program
+* description 'function that prints alphabet in lowcase'
+*Return: always (0) on success
+*/
+void print_alphabet_x10(void);
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+		_putchar('\n');
+
+
+
+	}
+	return (0);
+
+
+
+
+
+
+
+
+
+}
