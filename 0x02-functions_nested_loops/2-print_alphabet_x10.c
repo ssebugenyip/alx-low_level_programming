140c76f37b94e4896d27d57bb5ce56d3af88c90f
ssebugenyip <ssebugenyipatric7@gmail.com>
2022-10-18 12:50:10 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	int count = 0;
 	char letter = 'a';
 
-	while (count++ < 9)
+	while (count++ <= 9)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
