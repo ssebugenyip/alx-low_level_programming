8c71718ca613dcd0dd128ba73eaafaefa1c2aebe
ssebugenyip <ssebugenyipatric7@gmail.com>
2022-10-20 18:33:23 -0700

    multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*main - starting point of the program
+*positive_or_negative - tests for positive or negative integer
+*Return: always o for success
+*/
+int main(void)
+{
+	int x;
+	x = 0;
+	positive_or_negative(x);
+	
+	return (0)
+
+
+
+
+
+
+}
