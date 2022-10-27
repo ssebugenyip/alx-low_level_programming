a108e8069a7996fafe9cd3c81ed3da67d3973c7b
ssebugenyip <ssebugenyipatric7@gmail.com>
2022-10-27 13:01:30 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*_strncpy - program that copies a string
+*@dest: the string to be copied to
+*@src:the string source to be copied
+*@n: the number of bytes to be copied from src
+*Return: a pointer to the string dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0;
+	int source_length = 0;
+
+	while (src[index++])
+		source_length++;
+
+	for (index = 0; source[index] && index < n; index++)
+		dest[index] = source[index];
+
+	for (index = source_length; index < n; index++)
+		dest[index] = '\0';
+
+
+	return (dest);
+}
